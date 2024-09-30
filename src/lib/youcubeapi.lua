2570959a907d30b096971a117d392f314c291e5f
Mysticloud <admin@mysticlouds.com>
2024-09-30 13:10:16 +0100

    Update youcubeapi.lua

--- src/lib/youcubeapi.lua
+++ src/lib/youcubeapi.lua
@@ -32,7 +32,7 @@ end
 -- Look at the [Documentation](https://commandcracker.github.io/YouCube/) for moor information
 -- Contact the server owner on Discord, when the server is down
 local servers = {
-    "ws://127.0.0.1:5000", -- Your server!
+    "ws://167.235.32.44:5000", -- Your server!
     "wss://youcube.knijn.one", -- By EmmaKnijn#0043
     "wss://youcube.onrender.com", -- By Commandcracker#8528
 }
